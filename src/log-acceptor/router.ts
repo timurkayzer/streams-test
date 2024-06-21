83a90cfb5d3480cce5d0b8cdce76adc24ade6870
Timur Kaiser <timurkayzer@gmail.com>
2024-06-21 12:45:25 +0500

    stream with switch to backup stream

--- src/log-acceptor/router.ts
+++ src/log-acceptor/router.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { streamService } from "./stream-service";
+import { transformLogsStream } from "./transform-stream";
 
 export const logsRouter = Router();
-logsRouter.post("/", (req, res) => {
-	streamService.write(JSON.stringify(req.body));
-	if (req.body.action === "unblock") {
-		streamService.uncork();
-	}
+// logsRouter.post("/", (req, res) => {
+// 	streamService.write(JSON.stringify(req.body));
+// 	if (req.body.action === "unblock") {
+// 		streamService.uncork();
+// 	}
+// 	res.status(200).send({});
+// });
+
+logsRouter.post("/transform", (req, res) => {
+	transformLogsStream.write(JSON.stringify(req.body));
 	res.status(200).send({});
 });
--- src/log-acceptor/transform-stream.ts
+++ src/log-acceptor/transform-stream.ts
@@ -1,18 +1,49 @@
 import { Transform, TransformCallback } from "node:stream";
 import fs from "fs";
+import { streamService } from "./stream-service";
 
 // print and put incoming data into file
 export class TransformLogsStream extends Transform {
 	private fileStream: fs.WriteStream;
+	private state: "success" | "fail" = "success";
 	constructor(outputFilePath: string) {
 		super();
+		this.pipe(streamService);
 		this.fileStream = fs.createWriteStream(outputFilePath);
 	}
 	_transform(
 		chunk: any,
 		encoding: BufferEncoding,
 		callback: TransformCallback,
-	) {}
+	) {
+		console.log(chunk.toString());
+		try {
+			const fail = Math.random() * 100 < 10;
+			const resultData = JSON.parse(chunk.toString());
+			if (!fail && this.state === "success") {
+				console.log("success");
+				resultData.state = "success";
+			} else {
+				resultData.state = "fail";
+				if (this.state === "success") {
+					this.state = "fail";
+					console.log("fail");
+					this.cork();
+					this.unpipe(streamService);
+					this.pipe(this.fileStream);
+					this.uncork();
+				} else {
+					console.log("still fail");
+				}
+			}
+			this.push(JSON.stringify(resultData));
+			callback();
+			return true;
+		} catch (e) {
+			console.error(e);
+			callback(e as Error);
+		}
+	}
 }
 
 export const transformLogsStream = new TransformLogsStream(
